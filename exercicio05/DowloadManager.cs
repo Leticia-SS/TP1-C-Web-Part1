0800e8fefa1834ced8a452a3653e8350815da088
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-09 16:00:35 -0300

    exercicio 05

---
+++ exercicio05/DowloadManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio05
+{
+    internal class DowloadManager
+    {
+        public event EventHandler<string> downloadCompleted;
+
+        public void StartDownload(string fileName, int dowloadTimeInSeconds)
+        {
+            Console.WriteLine($"Iiniciando o download do arquivo: {fileName}");
+            Console.WriteLine($"Tempo estimado para o download: {dowloadTimeInSeconds} segundos");
+
+            Thread.Sleep(dowloadTimeInSeconds * 1000);
+
+            OnDownloadCompleted("Download concluído com sucesso!");
+        }
+        
+        public void OnDownloadCompleted(string message)
+        {
+            downloadCompleted?.Invoke(this, message);
+        }
+
+    }
+}
---
+++ exercicio05/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using exercicio05;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DowloadManager dowloadManager = new DowloadManager();
+
+        dowloadManager.downloadCompleted += (sender, message) =>
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+            Console.WriteLine("Notificação: O download foi concluído e está pronto para uso.");
+        };
+
+        Console.Write("Digite o nome do arquivo para download: ");
+        string file = Console.ReadLine();
+
+        Console.Write("Digite o tempo de simulação (segundos): ");
+        
+        if (int.TryParse(Console.ReadLine(), out int seconds))
+        {
+            new Thread(() =>
+            {
+                dowloadManager.StartDownload(file, seconds);
+            }).Start();
+
+            Console.WriteLine("O download está em progresso... Você pode continuar usando o sistema.");
+        }
+        else
+        {
+            Console.WriteLine("Tempo inválido. Use apenas números inteiros.");
+        }
+    }
+}
