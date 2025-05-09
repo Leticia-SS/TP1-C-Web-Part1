6629695ca0a72ee06c9abafdd0ca7c0539ee4b37
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-09 15:00:17 -0300

    exercicio 03

---
+++ exercicio03/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Digite a base do retangulo: ");
+        double baseRetangulo = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura do retangulo: ");
+        double alturaRetangulo = double.Parse(Console.ReadLine());
+
+        Func<double, double, double> calcularArea = (baseRetangulo, alturaRetangulo) => baseRetangulo * alturaRetangulo;
+
+        double areaRetangulo = calcularArea(baseRetangulo, alturaRetangulo);
+        Console.WriteLine($"A área do retângulo é: {areaRetangulo}");
+    }
+}
