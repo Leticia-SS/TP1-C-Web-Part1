cf3484c3c5c327eddcca4dcd88f2e38399f8ab1c
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-09 16:20:56 -0300

    exercicio 06

---
+++ exercicio06/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio06
+{
+    internal class Logger
+    {
+        public void LogToConsole(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"[CONSOLE] {DateTime.Now}: {message}");
+            Console.ResetColor();
+        }
+
+        public void LogToFile(string message)
+        {
+            string logEntry = $"[FILE] {DateTime.Now}: {message}";
+            File.AppendAllText("app.log", logEntry + Environment.NewLine);
+            Console.WriteLine($"Log gravado em arquivo: app.log");
+        }
+
+        public void LogToDatabase(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"[DATABASE] {DateTime.Now}: Registro simulado no BD - {message}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ exercicio06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using exercicio06;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Logger logger = new Logger();
+
+        Action<string> logToConsole = logger.LogToConsole;
+
+        logToConsole += logger.LogToFile;
+        logToConsole += logger.LogToDatabase;
+
+        while (true)
+        {
+            Console.Write("Digite uma mensagem para logar (ou 'sair' para encerrar): ");
+            string input = Console.ReadLine();
+            
+            if (input.ToLower() == "sair")
+            {
+                break;
+            }
+            
+            logToConsole(input);
+            Console.WriteLine("Todos os logs foram processados.\n");
+        }
+    }
+}
