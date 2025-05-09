85b1d9f6c6e9e6cdc4c9f1eebb0486b264063b40
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-09 15:29:01 -0300

    exercicio 04

---
+++ exercicio 04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using exercicio_04;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TemperatureSensor sensor = new TemperatureSensor();
+
+        sensor.temperatureExceeded += (sender, message) =>
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        };
+
+        while (true)
+        {
+            Console.Write("Digite a temperatura atual (ou 'sair' para encerrar): ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "sair")
+                break;
+
+            if (double.TryParse(input, out double temperature))
+            {
+                sensor.SetTemperature(temperature);
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida. Digite um número ou 'sair'.");
+            }
+        }
+    }
+}
+
---
+++ exercicio 04/TemperatureSensor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio_04
+{
+    internal class TemperatureSensor
+    {
+        public event EventHandler<string> temperatureExceeded;
+        private double _temperature;
+
+        public void SetTemperature(double temperature)
+        {
+            _temperature = temperature;
+            if (_temperature > 100)
+            {
+                OnTemperatureExceeded($"ALERTA! Temperatura excedeu o limite: {_temperature}");
+            }
+        }
+
+        public void OnTemperatureExceeded(string message)
+        {
+            temperatureExceeded?.Invoke(this, message);
+        }
+    }
+
+}
