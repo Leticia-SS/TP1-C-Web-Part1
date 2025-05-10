872be7d661ec39765bc3513dfa62e613432464e3
Leticia-SS <leticia.saraiva013@gmail.com>
2025-05-09 22:45:49 -0300

    exercicio 11

---
+++ exercicio11/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<string, string, string> formatador = (nome, sobrenome) =>
+        {
+            var resultado = nome + " " + sobrenome;
+            Console.WriteLine("Após concatenar: " + resultado);
+            return resultado;
+        };
+
+        formatador += (nomeCompleto, _) =>
+        {
+            var resultado = nomeCompleto.ToUpper();
+            Console.WriteLine("Após converter para maiúsculas: " + resultado);
+            return resultado;
+        };
+
+        formatador += (nomeCompleto, _) =>
+        {
+            var resultado = nomeCompleto.Replace(" ", "");
+            Console.WriteLine("Após remover espaços: " + resultado);
+            return resultado;
+        };
+
+        Console.WriteLine("Digite o nome:");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Digite o sobrenome:");
+        string sobrenome = Console.ReadLine();
+
+        Console.WriteLine("\nProcessamento:");
+        string resultadoFinal = formatador(nome, sobrenome);
+
+        Console.WriteLine("\nResultado final: " + resultadoFinal);
+    }
+}
